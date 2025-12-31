0a652ace1ea9fbd54dd9127a93c28419c2fa3320
takumi <tkmttkm322@icloud.com>
2025-12-31 12:56:21 +0900

    [add]psalm false

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -2,9 +2,10 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      ltex = {
-        filetypes = { "tex", "plaintex", "text" }, -- markdown を外す
-      },
+    	psalm = false,
+        ltex = {
+        	filetypes = { "tex", "plaintex", "text" }, -- markdown を外す
+        },
     },
   },
 }
