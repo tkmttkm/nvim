fe8f6d0daf6714f6526ead2f4a0541c82835c358
takumi <tkmttkm322@icloud.com>
2026-02-13 23:08:30 +0900

    [update]japanese
    codexを日本語返答に

--- lua/plugins/codex.lua
+++ lua/plugins/codex.lua
@@ -1,6 +1,13 @@
 return {
     "rhart92/codex.nvim",
-    opts = {
-        split = 'vertical',
-    }
+    config = function()
+        require("codex").setup({
+            system_prompt = [[
+                                You must always respond in Japanese.
+                                You must correctly understand Japanese technical specifications.
+                                When generating code, include Japanese comments.
+                                Do not switch to English unless explicitly instructed.
+                            ]]
+        })
+    end,
 }
